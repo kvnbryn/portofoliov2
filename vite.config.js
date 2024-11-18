7bf43814db3a63cc06d37f932d22ec49783c8cbf
kvnbryn <kvnbryank@gmail.com>
2024-11-18 17:05:28 +0800

    rate limit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import mdx from '@mdx-js/rollup'
 import {
   vitePlugin as remix,
   cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
-} from '@remix-run/dev';
-import { defineConfig } from 'vite';
-import jsconfigPaths from 'vite-jsconfig-paths';
-import mdx from '@mdx-js/rollup';
-import remarkFrontmatter from 'remark-frontmatter';
-import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
-import rehypeImgSize from 'rehype-img-size';
-import rehypeSlug from 'rehype-slug';
-import rehypePrism from '@mapbox/rehype-prism';
+} from '@remix-run/dev'
+import rehypeImgSize from 'rehype-img-size'
+import rehypeSlug from 'rehype-slug'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+import { defineConfig } from 'vite'
+import jsconfigPaths from 'vite-jsconfig-paths'
 
 const isStorybook = process.argv[1]?.includes('storybook');
 
 export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
   build: {
-    assetsInlineLimit: 1024,
+    assetsInlineLimit: 0,
   },
   server: {
     port: 7777,
